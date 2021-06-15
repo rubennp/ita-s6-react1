e9922ed132efd9fcf344be8ecc863381b6dc4c06
Rubèn Nieto <ruben.np@me.com>
2021-06-15 19:12:30 +0200

    Fi ex2

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+// import "../assets/css/App.css";
+import Book from "./Book";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Book />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Book extends Component {
+    render() {
+        return (
+            <div>
+                <p>Viatge a la lluna</p>
+            </div>
+        )
+    }
+};
+
+export default Book;
